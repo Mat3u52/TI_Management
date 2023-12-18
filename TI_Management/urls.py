d66f66e1bf98c4f2ae31afda325d032f3a7a92ee
Mateusz <kolodziej2007@tlen.pl>
2023-12-18 05:46:34 +0100

    added urls for logged_out

--- TI_Management/urls.py
+++ TI_Management/urls.py
@@ -18,9 +18,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from TI_Management_app.views import Image, ImageDisplay
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
     path('', include('TI_Management_app.urls')),
     path('images/', Image.as_view(), name='image'),
     path('images/<int:pk>/', ImageDisplay.as_view(), name="image_display"),
