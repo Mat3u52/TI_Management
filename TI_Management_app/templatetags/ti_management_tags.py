a3b947074e99f0af0f8895a88a2c0fbab4b1b7b1
Mateusz <kolodziej2007@tlen.pl>
2024-06-07 04:15:57 +0200

    finance

--- TI_Management_app/templatetags/ti_management_tags.py
+++ TI_Management_app/templatetags/ti_management_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from ..models import MembersZZTI
+from ..models import MembersZZTI, BankStatement
 from django.contrib.auth.models import User
 from datetime import datetime
 import os
@@ -15,7 +15,8 @@ def admin_exist(member_nr):
 
 
 @register.filter(name='show_timestamp')
-def show_timestamp(timestamp):
+# def show_timestamp(timestamp):
+def show_timestamp():
     current_time = datetime.utcnow()
     timestamp = int(current_time.timestamp())
     return timestamp
@@ -26,19 +27,44 @@ def comma_to_period(value):
     return str(value).replace(',', '.')
 
 
-@register.filter
+@register.filter(name='filename')
 def filename(value):
     return os.path.basename(value)
 
 
-@register.filter
+@register.filter(name='month_name')
 def month_name(month_number):
     return calendar.month_name[month_number]
 
 
-@register.filter
+@register.filter(name='to_int')
 def to_int(value):
     try:
         return int(value)
     except (ValueError, TypeError):
         return value
+
+
+@register.simple_tag
+def get_months_until_now(year=None):
+    current_date = datetime.now()
+    current_year = current_date.year
+    current_month = current_date.month
+
+    months = [
+        "Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec",
+        "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"
+    ]
+    numeric_months = list(range(1, 13))
+
+    if year is None or year >= current_year:
+        months_until_now = [(months[i], numeric_months[i]) for i in range(current_month)]
+    else:
+        months_until_now = [(months[i], numeric_months[i]) for i in range(12)]
+
+    return months_until_now
+
+
+@register.filter(name='record_exist')
+def record_exist(year, month):
+    return BankStatement.objects.filter(year_bank_statement=year, month_bank_statement=month).first()
